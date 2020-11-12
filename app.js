3c8ec65f67f5aa56983e64d32aee6a50a095cd2c
hutamy <hutamytriesthi@gmail.com>
2020-11-13 01:12:58 +0700

    Add helper random logo

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const cors = require('cors');
 const Routes = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 const port = process.env.PORT || 3000
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 
 
 app.use(cors())
@@ -13,9 +15,12 @@ app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
 app.use('/', Routes)
-
 app.use(errorHandler)
 
-app.listen(port,()=>{
+io.on('connection', (socket) => {
+  console.log('a user connected')
+})
+
+http.listen(port,()=>{
   console.log(`App running in http://localhost:${port}`);
 })
