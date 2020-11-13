dacc41f20c7a20fee6fd90eaf22d8fec5f7cfa86
Mochaa <mochamadzulfikar@gmail.com>
2020-11-13 13:29:06 +0700

    add change question handler

--- helpers/randomLogo.js
+++ helpers/randomLogo.js
@@ -1,11 +1,11 @@
 const data = [
     {
         name: 'youtube', 
-        imager_url: 'https://i.imgur.com/w5AOsXU.jpg'
+        image_url: 'https://i.imgur.com/w5AOsXU.jpg'
     },
     {
         name: 'bango',
-        imager_url: 'https://i.imgur.com/6wuTmGK.jpg'
+        image_url: 'https://i.imgur.com/6wuTmGK.jpg'
     },
     {
         name: 'starbuck',
@@ -64,3 +64,4 @@ function randomLogo () {
     return data[random]
 }
 
+module.exports = randomLogo
